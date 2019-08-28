902c271d5215b8800545ea4c60d78fbeec9541dc
Ted Osuch <ted@osuch.com>
2019-08-28 10:40:49 +0100

    added icons to supplement icons

--- db/vetpractice.sql
+++ db/vetpractice.sql
@@ -27,7 +27,7 @@ CREATE TABLE pets (
   name VARCHAR(255),
   date_of_birth VARCHAR(255),
   treatment_notes TEXT,
-  vet_id INT8 REFERENCES vets(id) ON DELETE CASCADE,
-  pet_type_id INT8 REFERENCES pettypes(id) ON DELETE CASCADE,
+  vet_id INT8 REFERENCES vets(id),
+  pet_type_id INT8 REFERENCES pettypes(id),
   owner_id INT8 REFERENCES owners(id) ON DELETE CASCADE
 );
